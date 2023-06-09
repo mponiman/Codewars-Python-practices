fb1f1adef52d7ce07b43bf96c76e748fb180ba99
mponiman <67168162+mponiman@users.noreply.github.com>
2023-06-09 11:18:14 -0400

    Create 6 kyu - Break camelCase.py

---
+++ 6 kyu - Break camelCase.py
@@ -0,0 +1,22 @@
+"""
+Instructions
+Complete the solution so that the function will break up camel casing, using a space between words.
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+"""
+
+def solution(s):
+    result = ""
+    for letter in s:
+        if letter != letter.upper():
+            result += letter
+        else:
+            result += " " + letter
+    return result
+  
+  "OR"
+  
+  def solution(s):
+    return "".join(" " + l if l.isupper() else l for l in s)
